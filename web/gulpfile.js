ff7b0995a5c727a7a2778bdca60a47a6c0b00c84
Matt Todd <matt.c.todd@gmail.com>
2015-11-17 06:32:06 -0600

    fixed webserver

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,14 +1,11 @@
-var gulp = require('gulp');
-var webserver = require('gulp-webserver');
- 
+var gulp = require('gulp')
+  , webserver = require('gulp-webserver');
+
 gulp.task('webserver', function() {
   gulp.src('public')
     .pipe(webserver({
         port: 8000,
         livereload: true,
-        path: 'public',
-        directoryListing: true,
-        open: true,
-        fallback: 'index.html',
+        open: true
       }));
 });
