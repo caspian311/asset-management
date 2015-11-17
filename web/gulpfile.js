c54ba99f06f38dbe24f4f542ea69c0ceb44e894a
Matt Todd <matt.c.todd@gmail.com>
2015-11-17 06:35:08 -0600

    using wiredep to pull in bower dependencies

--- web/gulpfile.js
+++ web/gulpfile.js
@@ -1,5 +1,15 @@
 var gulp = require('gulp')
-  , webserver = require('gulp-webserver');
+  , webserver = require('gulp-webserver')
+  , wiredep = require('wiredep').stream;
+
+gulp.task('bower', function () {
+  gulp.src('./public/index.html')
+    .pipe(wiredep({
+      optional: 'configuration',
+      goes: 'here'
+    }))
+    .pipe(gulp.dest('./public'));
+});
 
 gulp.task('webserver', function() {
   gulp.src('public')
