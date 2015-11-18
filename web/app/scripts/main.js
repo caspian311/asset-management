fd6a8b90537e84733877909dd4131695bb5ce55b
Matt Todd <matt.c.todd@gmail.com>
2015-11-18 07:14:45 -0600

    added angular and a basic controller

--- web/app/scripts/main.js
+++ web/app/scripts/main.js
@@ -1,1 +1,14 @@
-console.log('\'Allo \'Allo!');
+(function() {
+
+var app = angular.module('app', []);
+
+app.controller('LoginCtrl', function($scope) {
+  $scope.name = 'monkey';
+  $scope.foos = [{ foo: 'bar' }, { foo: 'baz' }];
+
+  $scope.signin = function() {
+    console.log('logging in with: ' + $scope.email + '/' + $scope.password);
+  }
+});
+
+})();
