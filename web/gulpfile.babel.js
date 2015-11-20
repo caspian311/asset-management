f1256186ab3c9e2e99a151a5876ac10a08102c2f
Matt Todd <matt.c.todd@gmail.com>
2015-11-20 05:23:04 -0600

    removed old in-browser test

--- web/gulpfile.babel.js
+++ web/gulpfile.babel.js
@@ -105,21 +105,6 @@ gulp.task('serve:dist', () => {
   });
 });
 
-gulp.task('serve:test', () => {
-  browserSync({
-    notify: false,
-    port: 9000,
-    ui: false,
-    server: {
-      baseDir: 'test',
-      routes: {
-        '/bower_components': 'bower_components'
-      }
-    }
-  });
-
-  gulp.watch('test/spec/**/*.js').on('change', reload);
-});
 
 // inject bower components
 gulp.task('wiredep', () => {
