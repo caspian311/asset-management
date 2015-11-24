f5a339f924ca362b26e9cccdb5239cd9cc7c5a5c
Matt Todd <matt.c.todd@gmail.com>
2015-11-24 06:09:39 -0600

    fixed issue with ngRoute not being pulled in correctly

--- web/app/scripts/login/login.module.js
+++ web/app/scripts/login/login.module.js
@@ -1,7 +1,7 @@
 (function() {
   'use strict';
 
-  angular.module('assets.login', [])
+  angular.module('assets.login', ['ngRoute'])
     .config(['$routeProvider', function($routeProvider) {
         $routeProvider.when('/login', {
           templateUrl: 'scripts/login/login.html',
