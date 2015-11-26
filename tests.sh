3e7c14dab6af9ee44fa1ef7f380cff3092d84c3b
Matt Todd <matt.c.todd@gmail.com>
2015-11-26 14:54:49 -0600

    single place to run all tests

---
+++ tests.sh
@@ -0,0 +1,23 @@
+#!/bin/bash -e
+
+echo "Running web tests..."
+
+pushd web &> /dev/null
+  karma start karma.conf-ci.js
+popd &> /dev/null
+
+echo "Running auth_service tests..."
+
+pushd auth_service &> /dev/null
+  bundle &> /dev/null
+  bundle exec rake db:migrate &> /dev/null
+  bundle exec rspec
+popd &> /dev/null
+
+echo "Running user_service tests..."
+
+pushd user_service &> /dev/null
+  bundle &> /dev/null
+  bundle exec rake db:migrate &> /dev/null
+  bundle exec rspec
+popd &> /dev/null
