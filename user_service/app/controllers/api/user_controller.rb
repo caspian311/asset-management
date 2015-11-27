f9931706717c072662172893c12c301de4697a27
Matt Todd <matt.c.todd@gmail.com>
2015-11-27 09:36:41 -0600

    user service implemented

--- user_service/app/controllers/api/user_controller.rb
+++ user_service/app/controllers/api/user_controller.rb
@@ -1,6 +1,15 @@
 class Api::UserController < ApplicationController
   respond_to :json
 
+  def index
+    user = User.find_by crendetials
+    if user
+      render json: user, status: 200
+    else
+      render nothing: true, status: 404
+    end
+  end
+
   def create
     status = 400
     status = 200 if User.create(user_data).valid? 
@@ -8,6 +17,9 @@ class Api::UserController < ApplicationController
   end
 
   private
+  def crendetials
+    params.permit :email, :password
+  end
 
   def user_data
     params.permit :first_name, :last_name, :email, :password
--- user_service/spec/requests/user_controller_spec.rb
+++ user_service/spec/requests/user_controller_spec.rb
@@ -8,35 +8,75 @@ describe Api::UserController do
   let(:valid_user) { { first_name: first_name, last_name: last_name, email: email, password: password } }
   let(:invalid_user) { {} }
 
-  context 'valid user data' do
-    it 'should create the user' do
-      post api_user_index_path, valid_user
+  context 'POST /api/user' do
+    context 'valid user data' do
+      it 'should create the user' do
+        post api_user_index_path, valid_user
 
-      expect(User.count).to eq(1)
-      expect(User.last.first_name).to eq(first_name)
-      expect(User.last.last_name).to eq(last_name)
-      expect(User.last.email).to eq(email)
-      expect(User.last.password).to eq(password)
+        expect(User.count).to eq(1)
+        expect(User.last.first_name).to eq(first_name)
+        expect(User.last.last_name).to eq(last_name)
+        expect(User.last.email).to eq(email)
+        expect(User.last.password).to eq(password)
+      end
+
+      it 'should respond with OK' do
+        post api_user_index_path, valid_user
+
+        expect(response.status).to eq(200)
+      end
     end
 
-    it 'should respond with OK' do
-      post api_user_index_path, valid_user
+    context 'invalid user data' do
+      it 'should not create the user' do
+        post api_user_index_path, invalid_user
 
-      expect(response.status).to eq(200)
+        expect(User.count).to eq(0)
+      end
+
+      it 'should respond with bad request' do
+        post api_user_index_path, invalid_user
+
+        expect(response.status).to eq(400)
+      end
     end
   end
 
-  context 'invalid user data' do
-    it 'should not create the user' do
-      post api_user_index_path, invalid_user
+  context 'GET /api/user' do
+    let(:email) { 'email@address.com' }
+    let(:password) { 'password' }
+    let(:valid_credentials) { { email: email, password: password } }
 
-      expect(User.count).to eq(0)
+    context 'requeted user exists' do
+      let(:first_name) { 'first' }
+      let(:last_name) { 'last' }
+      let!(:user) { create :user, first_name: first_name, last_name: last_name, email: email, password: password }
+
+      it 'should return a 200' do
+        get api_user_index_path, valid_credentials
+
+        expect(response).to have_http_status(200)
+      end
+
+      it 'should return the user data' do
+        get api_user_index_path, valid_credentials
+
+        expect(parse_body['first_name']).to eq(first_name)
+        expect(parse_body['last_name']).to eq(last_name)
+        expect(parse_body['email']).to eq(email)
+      end
     end
 
-    it 'should respond with bad request' do
-      post api_user_index_path, invalid_user
+    def parse_body
+      JSON.parse(response.body)
+    end
 
-      expect(response.status).to eq(400)
+    context 'requeted user does not exist' do
+      it 'return a 404' do
+        get api_user_index_path, valid_credentials
+
+        expect(response).to have_http_status(404)
+      end
     end
   end
 end
