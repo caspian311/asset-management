5a6c6556797f12f499231bf7ef47769065138b66
Matt Todd <matt.c.todd@gmail.com>
2015-12-06 13:57:55 -0600

    ensure that we put the data in the backend

--- web/app/scripts/profile/profile.controller.js
+++ web/app/scripts/profile/profile.controller.js
@@ -5,18 +5,28 @@
     .controller('ProfileController', ['$scope', '$http', '$cookies', 'endpoints', ProfileController]);
 
   function ProfileController(scope, http, cookies, endpoints) {
-    http.get(endpoints.userUrl(cookies.getObject('user').id)).success(function(user) {
-      scope.firstName = user.firstName;
-      scope.lastName = user.lastName;
-      scope.primaryPhone = user.primaryPhone;
-      scope.secondaryPhone = user.secondaryPhone;
-      scope.address = user.address;
-      scope.city = user.city;
-      scope.state = user.state;
-      scope.zip = user.zip;
-      scope.password = user.password;
+    http.get(userUrl()).success(function(user) {
+      scope.user = {
+        firstName: user.firstName,
+        lastName: user.lastName,
+        primaryPhone: user.primaryPhone,
+        secondaryPhone: user.secondaryPhone,
+        address: user.address,
+        city: user.city,
+        state: user.state,
+        zip: user.zip,
+        password: user.password
+      };
     }).error(function() {
       scope.errorMessage = 'Failed to load user\'s profile information.';
     });
+
+    scope.update = function() {
+      http.put(userUrl(), scope.user).success(function() {});
+    };
+
+    function userUrl() {
+      return endpoints.userUrl(cookies.getObject('user').id);
+    }
   }
 })();
--- web/test/profile/profile.controller.test.js
+++ web/test/profile/profile.controller.test.js
@@ -12,7 +12,6 @@
       userUrl = $injector.get('endpoints').userUrl;
       cookies = { getObject: sinon.stub() };
       controller = $controller;
-
     }));
 
     afterEach(function() {
@@ -20,8 +19,18 @@
       httpBackend.verifyNoOutstandingRequest();
     });
 
+    var userId = 123
+      , firstName = 'first name'
+      , lastName = 'lastName'
+      , primaryPhone = 'primaryPhone'
+      , secondaryPhone = 'secondaryPhone'
+      , address = 'address'
+      , city = 'city'
+      , state = 'state'
+      , zip = 'zip'
+      , password = 'password';
+
     describe('initially', function() {
-      var userId = 123;
 
       beforeEach(function() {
         cookies.getObject.withArgs('user').returns({ id: userId });
@@ -30,15 +39,6 @@
       });
 
       it('pulls profile data from backend', function() {
-          var firstName = 'first name'
-            , lastName = 'lastName'
-            , primaryPhone = 'primaryPhone'
-            , secondaryPhone = 'secondaryPhone'
-            , address = 'address'
-            , city = 'city'
-            , state = 'state'
-            , zip = 'zip'
-            , password = 'password';
           httpBackend.when('GET', userUrl(userId)).respond(200, 
             { 
               firstName: firstName,
@@ -53,22 +53,61 @@
             });
           httpBackend.flush();
 
-          expect(scope.firstName).to.eq(firstName);
-          expect(scope.lastName).to.eq(lastName);
-          expect(scope.secondaryPhone).to.eq(secondaryPhone);
-          expect(scope.primaryPhone).to.eq(primaryPhone);
-          expect(scope.address).to.eq(address);
-          expect(scope.city).to.eq(city);
-          expect(scope.state).to.eq(state);
-          expect(scope.zip).to.eq(zip);
-          expect(scope.password).to.eq(password);
+          expect(scope.user.firstName).to.eq(firstName);
+          expect(scope.user.lastName).to.eq(lastName);
+          expect(scope.user.secondaryPhone).to.eq(secondaryPhone);
+          expect(scope.user.primaryPhone).to.eq(primaryPhone);
+          expect(scope.user.address).to.eq(address);
+          expect(scope.user.city).to.eq(city);
+          expect(scope.user.state).to.eq(state);
+          expect(scope.user.zip).to.eq(zip);
+          expect(scope.user.password).to.eq(password);
       });
 
       it('shows error if cant retieve data', function() {
-          httpBackend.when('GET', userUrl).respond(404, { });
+          httpBackend.when('GET', userUrl(userId)).respond(404, { });
           httpBackend.flush();
 
           expect(scope.errorMessage).to.eq('Failed to load user\'s profile information.');
+      });
+    });
+
+    describe('#update', function() {
+      beforeEach(function() {
+        cookies.getObject.withArgs('user').returns({ id: userId });
+
+        controller('ProfileController', { $scope: scope, $cookies: cookies });
+
+        httpBackend.when('GET', userUrl(userId)).respond(200, { });
+        httpBackend.when('PUT', userUrl(userId)).respond(201, { });
+      });
+
+      it('send data to backend', function() {
+          scope.user = {};
+          scope.user.firstName = firstName;
+          scope.user.lastName = lastName;
+          scope.user.secondaryPhone = secondaryPhone;
+          scope.user.primaryPhone = primaryPhone;
+          scope.user.address = address;
+          scope.user.city = city;
+          scope.user.state = state;
+          scope.user.zip = zip;
+          scope.user.password = password;
+          
+          httpBackend.expect('PUT', userUrl(userId), {
+              firstName: firstName,
+              lastName: lastName,
+              primaryPhone: primaryPhone,
+              secondaryPhone: secondaryPhone,
+              address: address,
+              city: city,
+              state: state,
+              zip: zip,
+              password: password
+          });
+
+          scope.update();
+          httpBackend.flush();
       });
     });
   });
