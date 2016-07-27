35c6ad43bccf7ec7ce2d9a96d7ea7657b94e2b42
Matt Todd <matt.c.todd@gmail.com>
2016-07-27 11:11:42 -0500

    added database docker container

--- build.sh
+++ build.sh
@@ -6,13 +6,17 @@ docker rm -f $(docker ps -a | grep -v CONTAINER | awk '{print $1}')
 set -e
 
 echo 'build-images'
+docker build -t caspian311/asset-management-db:1.0 db
 docker build -t caspian311/asset-management-web:1.0 web
 
 echo 'create loggin directories'
 
 web_log_dir=$(mkdir -p logging/web ; cd logging/web &> /dev/null; pwd; cd - &> /dev/null)
+db_log_dir=$(mkdir -p logging/db ; cd logging/db &> /dev/null; pwd; cd - &> /dev/null)
+data_dir=$(mkdir -p data ; cd data &> /dev/null; pwd; cd - &> /dev/null)
 
 echo 'run-images'
+docker run -d --name asset-management-db -v $data_dir:/var/lib/mysql -v $db_log_dir:/var/log/mysql -p 3306:3306 caspian311/asset-management-db:1.0
 docker run -d --name asset-management-web -v $web_log_dir:/var/log/nginx -p 80:80 caspian311/asset-management-web:1.0
 
 docker ps
